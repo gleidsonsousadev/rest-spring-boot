6d0ed5f78d94513c010dc4265727813427cdfb4c
Gleidson Sousa <gleidsonsousadev@gmail.com>
2023-09-25 14:51:15 -0300

    First Steps in Java with Spring Boot 3

---
+++ FirstSteps/rest-spring-boot/src/test/java/br/com/tribo/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tribo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
